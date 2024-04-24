68887f1a223b17bc84886ab68dc93a5142c45dfa
joshyeom <jhyeom95@daum.net>
2024-04-24 20:54:23 +0900

    onSuccess, onError, enabled

--- src/ReactQuery.tsx
+++ src/ReactQuery.tsx
@@ -8,21 +8,36 @@ const fetchData = () => {
 }
 
 export const ReactQuery = () => {
-    const { isLoading, isFetching, data, isError, error}: any = useQuery(
+
+    const onSuccess = (data: any) => {
+      console.log("데이터 가져오기 후 사이드 이펙트 수행", data)
+    }
+
+    const onError = (error: any) => {
+      console.log("오류 발생 후 사이드 이펙트 수행", error)
+    }
+
+    const { isLoading, isFetching, data, isError, error, refetch}: any = useQuery(
       'get-product', fetchData,
         {
-          refetchInterval: 2000,
-          refetchIntervalInBackground: true,
+          enabled: false,
+          onSuccess: onSuccess,
+          onError: onError,
         }
     )
 
   if (isLoading) return <>Loading...</>
   if (isError) return <h2>{error.message}</h2>
 
-    console.log(isFetching)
 
   return (
     <>
+      <button
+        onClick={refetch}
+        className="py-2 px-4 border bg-slate-100 rounded-md"
+      >
+        fetch data
+      </button>
       <div className='text-4xl'>ReactQuery</div>
       <ul className='list-disc p-4'>
         {data &&
